869b13f4f7360b534875a56c362d845758309ef8
robertian <1qa2ws3ed>
2015-11-07 01:51:26 -0600

    adding scototaxis tracking in...

--- HSVTrack.py
+++ HSVTrack.py
@@ -41,8 +41,14 @@ def convertToHSV(frame):
     mask[:, :] = hsv[:, :]
     return mask
 
+def convertToHSVScoto(frame, blackbox, whitebox):
+    height, width, channels = frame.shape
+    blurred = cv2.blur(frame, (2, 2))
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    mask = np.zeros((height, width, 3), np.uint8)
+    mask[:, :] = hsv[:, :]
+    return maskb,maskw
 
-# function to determine where the fish started in the video
 def findStartingPoint(video, numberOfFrames):
     # the function will go numberOfFrames frames ahead in the video and do a subtraction
     # the hope is that the fish will have moved at some point during this time
@@ -54,27 +60,50 @@ def findStartingPoint(video, numberOfFrames):
         # grab a frame from the middle. Assume the screens have turned on by then
         if count == numberOfFrames / 2:
             hsv_middle = convertToHSV(frame)
+        if count == numberOfFrames / 4:
+            hsv_Q1 = convertToHSV(frmae)
         if count == numberOfFrames:
-            # grab the last frame in the numberOfFrames window
             hsv_end = convertToHSV(frame)
         count += 1
-        if count == 1:
-            print "initializing"
         print "." * (count % 20)
 
-    # now we have masked HSV photos from the beginning, middle, and end of the initialization period
-    # now we have to do some funky subtraction to get rid of the signal that results from the screen being turn on
     difference2 = cv2.subtract(hsv_end, hsv_initial)
     difference1 = cv2.subtract(hsv_end, hsv_middle)
-    difference3 = cv2.subtract(difference1, difference2)
-
-    # now we have what we want
-    thresh = cv2.inRange(difference3, np.array([0, 0, 0]), np.array([255, 255, 25]))
-    # need to invert the colors or else we run into trouble on our call to cv2.findContours
+    difference3 = cv2.subtract(hsv_Q1, hsv_end)
+    difference4 = cv2.subtract(difference1, difference3)
+    difference5 = cv2.subtract(difference2, difference2)
+    difference = cv2.subtract(difference5, difference4)
+    thresh = cv2.inRange(difference, np.array([0, 0, 0]), np.array([255, 255, 25]))
     invert = cv2.bitwise_not(thresh)
-    startingPoint = returnLargeContour(invert)
-    return (startingPoint)
+    return startingPoint
 
+def findStartingPointScoto(video, numberOfFrames):
+    # the function will go numberOfFrames frames ahead in the video and do a subtraction
+    # the hope is that the fish will have moved at some point during this time
+    count = 0
+    # I don't know a better way to jump ahead so many frames
+    while count <= numberOfFrames:
+        # read in the frame for each tick of the loop
+        ret, frame = video.read()
+        # grab a frame from the middle. Assume the screens have turned on by then
+        if count == numberOfFrames / 2:
+            hsv_middle = convertToHSV(frame)
+        if count == numberOfFrames / 4:
+            hsv_Q1 = convertToHSV(frmae)
+        if count == numberOfFrames:
+            hsv_end = convertToHSV(frame)
+        count += 1
+        print "." * (count % 20)
+
+    difference2 = cv2.subtract(hsv_end, hsv_initial)
+    difference1 = cv2.subtract(hsv_end, hsv_middle)
+    difference3 = cv2.subtract(hsv_Q1, hsv_end)
+    difference4 = cv2.subtract(difference1, difference3)
+    difference5 = cv2.subtract(difference2, difference2)
+    difference = cv2.subtract(difference5, difference4)
+    startingPointWhite = returnLargeContour(invert)
+    startingpointBlack = returnLargeContour(thresh)
+    return startingpointBlack, startingPointWhite
 
 # returns centroid from largest contour from a binary image
 def returnLargeContour(frame):
@@ -108,8 +137,36 @@ def returnLargeContour(frame):
             csv_writer.writerow((centroid_x, centroid_y, counter))
             return ((centroid_x, centroid_y))
 
-#def returnCentroid(frame, lastPosition):
-    #pass
+def returnLargeContoursScoto(frameb, framew):
+    potential_centroidsb = []
+    potential_centroidsw = []
+    # find all contours in the frame
+    contours = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+    print "number of contours: " + str(len(contours))
+
+    for z in contours:
+        # calculate some things
+        area = cv2.contourArea(z)
+        x, y, w, h = cv2.boundingRect(z)
+        aspect_ratio = float(w) / h
+        #the main filtering statement
+        if area > 60 and area < 10000:  #and aspect_ratio <= 2.0 and aspect_ratio >= 0.5
+            potential_centroids.append(z)
+            print area
+            print aspect_ratio
+
+    largestCon = sorted(potential_centroids, key=cv2.contourArea, reverse=True)[:1]
+
+    if len(potential_centroids) == 0:
+        csv_writer.writerow(("NA", "NA", counter))
+        return ()
+    else:
+        for j in largestCon:
+            m = cv2.moments(j)
+            centroid_x = int(m['m10'] / m['m00'])
+            centroid_y = int(m['m01'] / m['m00'])
+            csv_writer.writerow((centroid_x, centroid_y, counter))
+            return ((centroid_x, centroid_y))
 
 
 if '__name__'=='__main__':
@@ -131,16 +188,22 @@ if '__name__'=='__main__':
     ret, frame = cap.read()
     hsv_initial = convertToHSV(frame)
 
-    while (cap.isOpened()):
-
-
+    while ret:
         if args["scoto"]:
+            cv2.setMouseCallback('image', on_mouse2, None)
             blackbox =[]
             whitebox=[]
-            blackbx, bx1, bx2, bx3, bx4 = drawbox(blackbox)
-            cv2.polylines(frame, np.int32([blackbx]), 2, (0, 0, 255, 0))
-            whitebx, bx1, bx2, bx3, bx4 = drawbox(whitebox)
-            cv2.polylines(frame, np.int32([whitebx]), 2, (255, 255, 0, 0))
+            if len(blackbox) > 3:
+                blackbx, bx1, bx2, bx3, bx4 = drawbox(blackbox)
+                cv2.polylines(frame, np.int32([blackbx]), 1, (0, 0, 255, 0))
+                cv2.setMouseCallback('image', on_mouse2, None)
+                if len(whitebox) > 3:
+                    whitebx, wx1, wx2, wx3, wx4 = drawbox(whitebox)
+                    cv2.polylines(frame, np.int32([whitebx]), 1, (255, 255, 0, 0))
+                else:
+                    pass
+            else:
+                pass
 
             if counter == 0:
                 findStartingPoint(cap, 100)
@@ -158,10 +221,10 @@ if '__name__'=='__main__':
                 center = returnLargeContour(maskedInvert)
 
                 if center:
-                    cv2.circle(frame, center, 3, [0, 0, 255], -1)
+                    cv2.circle(frame, center, 3, [255, 0, 0], -1)
                 cv2.imshow('image', frame)
-                cv2.imshow('thresh', masked)
-                cv2.imshow('diff', difference)
+                #cv2.imshow('thresh', masked)
+                #cv2.imshow('diff', difference)
                 k = cv2.waitKey(1)
                 if k == 27:
                     break
